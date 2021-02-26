73d7150cdad22996c70076207872e3327a562668
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-02-26 22:02:18 +0900

    Update 11057.py
    이거 푸는중

--- BOJ/DP/11057.py
+++ BOJ/DP/11057.py
@@ -1,8 +1,22 @@
 value = int(input())
 
-dp = [[0 for x in range(11)] for x in range(1001)]
+dp = [[0 for x in range(12)] for x in range(1001)]
 
-dp[1] = [1,1,1,1,1,1,1,1,1,1]
+dp[1] = [1,1,1,1,1,1,1,1,1,1,10]
 
-for x in range(2, 10):
-    for j in range()
+
+
+for x in range(2, value+1, 1):
+    sum_value = 0
+    
+    for j in range(1, 10, 1):
+        temp_value = 0
+        for z in range(1, j+1):
+            temp_value += dp[x-1][z]
+        dp[x][j] = dp[x-1][-1] - temp_value
+       
+        sum_value += dp[x][j]
+
+    dp[x][-1] = sum_value
+
+print(dp[value][-1]%10007)
