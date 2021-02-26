c6e18713cb14ac58099c594c0f75b369503e1cbf
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-02-27 00:17:42 +0900

    8일차 DP
    11057
    
    풀긴 풀었는데
    DP로 안풀어도 되나??

--- BOJ/DP/11057.py
+++ BOJ/DP/11057.py
@@ -8,15 +8,27 @@ dp[1] = [1,1,1,1,1,1,1,1,1,1,10]
 
 for x in range(2, value+1, 1):
     sum_value = 0
-    
-    for j in range(1, 10, 1):
-        temp_value = 0
-        for z in range(1, j+1):
-            temp_value += dp[x-1][z]
-        dp[x][j] = dp[x-1][-1] - temp_value
-       
-        sum_value += dp[x][j]
 
+    for j in range(1, 11):
+        
+        temp_value = 0
+        
+        for z in range(1, j):
+            temp_value += dp[x-1][z]
+        
+        dp[x][j] = dp[x-1][-1] - temp_value
+        sum_value += dp[x][j]
+        
+    
     dp[x][-1] = sum_value
 
 print(dp[value][-1]%10007)
+
+
+# k=int(input())
+# result=1
+# for i in range(9+k,9,-1):
+#     result*=i
+# for i in range(1,k+1):
+#     result//=i
+# print(result%10007)
