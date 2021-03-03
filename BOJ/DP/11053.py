51b38a4a66c62f0c0fd808f0d33c3952d1098b49
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-03-04 01:12:11 +0900

    10-2 일차 DP
    얼추 이해했는데 역시 어려움

--- BOJ/DP/11053.py
+++ BOJ/DP/11053.py
@@ -1,12 +1,28 @@
-value = int(input())
+# value = int(input())
 
-answer = list(map(int, input().split(' ')))
-dp = [0 for i in range(value)]
+# answer = list(map(int, input().split(' ')))
+# dp = [0 for i in range(value)]
 
-for i in range(value):
-    for j in range(i):
-        if answer[i] > answer[j] and dp[i] < dp[j]:
-            dp[i] = dp[j]
-    dp[i]+=1
+# for i in range(value):
+#     for j in range(i):
+#         if answer[i] > answer[j] and dp[i] < dp[j]:
+#             dp[i] = dp[j]
+#     dp[i]+=1
 
-print(max(dp))    
+# print(max(dp))    
+
+
+
+
+n = int(input())
+
+numbers = list(map(int, input().split()))
+
+dp = [1] * n
+
+for x in range(1, n):
+    for y in range(x):
+        if numbers[x] > numbers[y]:
+            dp[x] = max(dp[x], dp[y]+1)
+
+print(max(dp))
