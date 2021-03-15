db9d5cafffb1967c10592cb3887e62340acca5d6
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-03-16 00:28:02 +0900

    14일차 PG 파이썬 강의
    입력받은 문자열에 가장 많이 반복된거 출력하기

---
+++ Programmers/파이썬을파이썬답게/12.py
@@ -0,0 +1,13 @@
+import collections
+my_list = input().strip()
+answer = collections.Counter(my_list)
+
+tempAnswer = dict(answer)
+
+realAnswer = [x for x, y in tempAnswer.items() if max(tempAnswer.values()) == y]
+
+print(''.join(sorted(realAnswer)))
+# print(answer[1]) # = 4
+# print(answer[3])  # = 3
+# print(answer[100]) # = 0
+
