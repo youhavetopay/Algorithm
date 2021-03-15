7ed3f86eaae398325152ac52d0c84c78c70a0d4c
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-03-16 00:31:13 +0900

    14-1일차 PG 파이썬 강의
    입력받은 리스트 중 짝수만 제곱해서 출력

---
+++ Programmers/파이썬을파이썬답게/13.py
@@ -0,0 +1,14 @@
+def solution(mylist):
+    answer = []
+    
+    for i in mylist:
+        if i%2 == 0:
+            answer.append(i**2)
+    
+    return answer
+
+
+def solution2(mylist):
+    answer = [i**2 for i in mylist: if i%2==0]
+    return answer
+
