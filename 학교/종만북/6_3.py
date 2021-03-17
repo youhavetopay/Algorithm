def0dbec5a987bcad0dfbd632495e1018040f05e
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-03-18 01:57:52 +0900

    종만북 6.3
    재귀로 푸는 건 이해 했으나
    반복문으로 푸는법은 못찾음
    또한 어떤식으로 접근해서 푸는지 잘 모르겠음

--- 학교/종만북/6_3.py
+++ 학교/종만북/6_3.py
@@ -17,55 +17,103 @@
 
 # print(tempList)
 
-ans = 0
-N , M = 0, 0
-areFriends = [[False for _ in range(10)] for _ in range(10)]
-havePair = [False for _ in range(10)]
+# ans = 0
+# N , M = 0, 0
+# areFriends = [[False for _ in range(10)] for _ in range(10)]
+# havePair = [False for _ in range(10)]
 
-def counting(n):
-    global ans
-    finished = True
-    frist = -1
-    for i in range(N):
-        if not havePair[i]:
-            finished = False
-            frist = i
-            break
+# def counting(n):
+#     global ans
+#     finished = True
+#     frist = -1
+#     for i in range(N):
+#         if not havePair[i]:
+#             finished = False
+#             frist = i
+#             break
     
-    if finished:
-        ans +=1
+#     if finished:
+#         ans +=1
         
-        return
+#         return
     
-    for j in range(frist+1, N):
-        if (not havePair[frist]) and (not havePair[j]) and areFriends[frist][j]:
-            havePair[frist] = True
-            havePair[j] = True
-            counting(n+1)
-            havePair[frist] = False
-            havePair[j] = False
+#     for j in range(frist+1, N):
+#         if (not havePair[frist]) and (not havePair[j]) and areFriends[frist][j]:
+#             havePair[frist] = True
+#             havePair[j] = True
+#             counting(n+1)
+#             havePair[frist] = False
+#             havePair[j] = False
     
-    return
+#     return
 
-tc = int(input())
+# tc = int(input())
 
-for t in range(1, tc+1):
-    mList = []
-    ans = 0
+# for t in range(1, tc+1):
+#     mList = []
+#     ans = 0
     
-    areFriends = [[False for _ in range(10)] for _ in range(10)]
-    havePair = [False for _ in range(10)]
+#     areFriends = [[False for _ in range(10)] for _ in range(10)]
+#     havePair = [False for _ in range(10)]
     
-    N, M = map(int, input().split())
-    tempList = list(map(int, input().split()))
-    for i in range(M):
-        a = tempList[2*i]
-        b = tempList[2*i+1]
-        areFriends[a][b] = True
-        areFriends[b][a] = True
+#     N, M = map(int, input().split())
+#     tempList = list(map(int, input().split()))
+#     for i in range(M):
+#         a = tempList[2*i]
+#         b = tempList[2*i+1]
+#         areFriends[a][b] = True
+#         areFriends[b][a] = True
     
-    counting(0)
+#     counting(0)
 
-    print(ans)
+#     print(ans)
 
     
+c = int(input())
+answers = []
+
+divFriendList= []
+
+for i in range(c):
+    n, m = map(int, input().split())
+
+    friendList = list(map(int, input().split()))
+    
+    answer = 0
+
+    divFriendList = [friendList[x*2 : x*2+2] for x in range(m)]
+
+    print(divFriendList)
+    # convList = []
+    # checkList = [-1 for _ in range(int(n/2))]
+    # remember = -1
+    # for x in range(m):
+    #     if divFriendList[x][0] not in convList and divFriendList[x][1] not in convList:
+    #         convList.append(divFriendList[x][0])
+    #         convList.append(divFriendList[x][1])
+    #         for y in range(int(n/2)):
+    #             if checkList[y] == -1:
+    #                 checkList[y] = x
+    #                 remember = y
+
+    #     if len(convList) >= int(n/2):
+    #         answer+=1
+    #         del convList[-1]
+    #         del convList[-1]
+
+        # for y in range(x+1, 9):
+        #     if (divFriendList[y][0] not in checkList) and (divFriendList[y][1] not in checkList):
+        #         checkList.append(divFriendList[y][0])
+        #         checkList.append(divFriendList[y][1])
+                
+        #         for z in range(y+1, 10):
+        #             print(checkList, divFriendList[z][0], divFriendList[z][1])
+        #             if (divFriendList[z][0] not in checkList) and (divFriendList[z][1] not in checkList):
+        #                 answer += 1
+                
+        #         del checkList[-1]
+        #         del checkList[-1]
+    
+    answers.append(answer)
+
+print(answers)
