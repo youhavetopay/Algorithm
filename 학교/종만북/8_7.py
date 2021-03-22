258a13c1a5b3b5b1bde0e573c07576797bd17136
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-03-22 23:39:37 +0900

    8_7 code edit
    코드 정답에 맞게 수정함

--- 학교/종만북/8_7.py
+++ 학교/종만북/8_7.py
@@ -1,20 +1,26 @@
 dp = [-1] *10002
-maxValue = 9999999999999
-
+maxValue = 987654321
+numList = ''
 def classify(a, b):
-
-    M = numList[a:(b-a+1)]
-    print(len(M))
+    
+    M = numList[a:b]
+    print(M)
     if M == str(M[0]*len(M)):
         return 1
 
-    progressive = True
-    for i in range(len(M)-1):
-        if (M[i+1] - M[i]) != (M[1] - M[0]):
-            progressive = False
-    
-    if (progressive and abs(M[1] - M[0])) == 1:
-        return 2
+    diff = int(M[1]) - int(M[0])
+    seq = True
+    for i in range(1,  b-a):
+        if int(M[i]) - int(M[i-1]) == diff: 
+            continue
+        seq = False
+        break
+
+    if seq:
+        if diff == -1 or diff == 1: 
+            return 2
+        else:
+            return
     
     alternating = True
     for i in range(len(M)):
@@ -23,15 +29,15 @@ def classify(a, b):
     
     if alternating: 
         return 4
-    if progressive : 
-        return 5
+
 
     return 10
 
         
 
 def memorize(begin):
-    if begin == len(numList):
+    
+    if begin >= len(numList):
         return 0
     ret = dp[begin]
 
@@ -42,7 +48,7 @@ def memorize(begin):
 
     for L in range(3, 6):
         if begin + L <= len(numList):
-            ret = min(ret, memorize(begin + L)+ classify(begin, begin + L -1))
+            ret = min(ret, classify(begin, begin + L)+memorize(begin + L))
     
     dp[begin] = ret
 
@@ -50,4 +56,4 @@ def memorize(begin):
 
 numList = input()
 
-print(numList[0:3])
+print(memorize(0))
