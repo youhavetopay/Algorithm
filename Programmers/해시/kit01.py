28897cb7faca8e76a653c8cb06f4e363623a6a98
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-03-23 10:21:38 +0900

    Update kit01.py

--- Programmers/해시/kit01.py
+++ Programmers/해시/kit01.py
@@ -1,22 +1,24 @@
 import collections
 
 
-# def solution(participant, completion):
-#     answer = ''
+def solution(participant, completion):
+    answer = ''
 
-#     participant.sort()
-#     completion.sort()
+    participant.sort()
+    completion.sort()
 
-#     for a, b in zip(participant, completion):
-#         if a != b:
-#             return a
+    # 정렬한 다음에 하나 하나 비교하기 다르거나 뒤에 남는게 답
+    for a, b in zip(participant, completion):
+        if a != b:
+            return a
 
-#     return participant[-1]
+    return participant[-1]
 
 
 
 
 def solution2(participant, completion):
+    # Counter : 리스트에 있는 거 딕셔너리 형으로 갯수 셈
     answer = collections.Counter(participant) - collections.Counter(completion)
     print(list(answer.keys()))
     return list(answer.keys())[0]
