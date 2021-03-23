a221085c34de95cb815d288c1dd823d91d7fd9a6
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-03-23 10:18:51 +0900

    PG 해시 완주하지 못한 선수
    리스트 비교해 남는 거 출력
    
    효율성에 점수 있어서 그냥 for문돌리면 안됨

--- Programmers/해시/kit01.py
+++ Programmers/해시/kit01.py
@@ -1,13 +1,30 @@
-def solution(participant, completion):
-    answer = ''
+import collections
 
+
+# def solution(participant, completion):
+#     answer = ''
+
+#     participant.sort()
+#     completion.sort()
+
+#     for a, b in zip(participant, completion):
+#         if a != b:
+#             return a
+
+#     return participant[-1]
+
+
+
+
+def solution2(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    print(list(answer.keys()))
+    return list(answer.keys())[0]
     
-
-    return answer
-
 
 list1 = ["leo", "kiki","kiki", "eden"]
 list2 = ["eden", "kiki","leo"]
 
-print(sorted(list1))
-print(sorted(list2))
+
+# print(solution(list1, list2))
+print(solution2(list1, list2))
