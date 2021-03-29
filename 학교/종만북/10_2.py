3a8887c65aa1bda710f7f6c7f96b771d8d5cb806
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-03-29 13:55:44 +0900

    종만북 10.3 그리디
    도시락 데우기
    풀이법을 알고 있었음

--- 학교/종만북/10_2.py
+++ 학교/종만북/10_2.py
@@ -1,13 +1,21 @@
 ## 도시락 데우기
-
+## 그리디 (탐욕)
 c = int(input())
 
 hotTimes = list(map(int, input().split()))
 
 eatTimes = list(map(int, input().split()))
+checkList = []
+hotTotalTime = 0
 
-totalTime = 0
+maxValue = max(eatTimes)
 
-checkList = [False] * c
+while maxValue != -1:
+    eatIndex = eatTimes.index(maxValue)
+    hotTotalTime = hotTotalTime + hotTimes[eatIndex]
+    checkList.append(hotTotalTime + eatTimes[eatIndex])
+    hotTimes[eatIndex] = -1
+    eatTimes[eatIndex] = -1
+    maxValue = max(eatTimes)
 
-print(max(hotTimes)+ sum(eatTimes))
+print(max(checkList))
