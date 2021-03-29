6365b61eca247b6824082093917af30f00031762
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-03-29 13:56:06 +0900

    Update kit03.py
    프로그래머스 스파이 변장? 해쉬
    
    틀림 ㅋㅋ

--- Programmers/고득점kit/해시/kit03.py
+++ Programmers/고득점kit/해시/kit03.py
@@ -1,24 +1,40 @@
 def solution(clothes):
-    answer = 0
-    return answer
+    dic1 = {}
+    emptyList = []
+    for tempList in clothes:
+        if dic1.get(tempList[1]):
+            dic1[tempList[1]].append(tempList[0])
+        else:
+            dic1[tempList[1]] = []
+            dic1[tempList[1]].append(tempList[0])
+
+    if len(dic1) == 1:
+        return len(dic1[list(dic1.keys())[0]])
+
+    else:
+        totalValue = 0
+        for i in range(0, len(list(dic1.values()))):
+            totalValue += len(list(dic1.values())[i])
+        tempValue = 1
+        for i in range(0, len(list(dic1.values()))):
+            tempValue *= len(list(dic1.values())[i]) 
+
+        return (totalValue+tempValue)
 
 
 
 list1 = [["yellowhat", "headgear"], ["bluesunglasses", "eyewear"], ["green_turban", "headgear"]]
 list2 = [["crowmask", "face"], ["bluesunglasses", "face"], ["smoky_makeup", "face"]]
-dic1 = {}
-emptyList = []
-for tempList in list1:
-    if dic1.get(tempList[1]):
-        dic1[tempList[1]].append(tempList[0])
-    else:
-        dic1[tempList[1]] = []
-        dic1[tempList[1]].append(tempList[0])
-        
+list3 = [["crowmask", "face1"], ["bluesunglasses", "face2"], ["smoky_makeup", "face3"]]
+list4 = [
+["a","aa"],
+["b","aa"],
+["c","aa"],
+["aa","bb"],
+["bb","bb"],
+["c_c","bb"],
+["aaa","cc"],
+["bbb","cc"],
+["ccc","cc"]
 
-# for key, value in dic1.items():
-#     if ' ' in value:
-#         dic1[key] = list(value.split(' '))
-
-
-
+]
