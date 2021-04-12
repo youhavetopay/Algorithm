7331fe29b448f0fe1783ea67b1c6b0e2ee1f62ab
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-04-12 21:21:55 +0900

    종만북 9.2 동적프로그래밍응용
    지금 코드 완성 못함 나중에 다시하기

--- 학교/종만북/9_2.py
+++ 학교/종만북/9_2.py
@@ -1,13 +1,13 @@
 #여행 짐 싸기
 
-n, w = map(int, input().split())
-BIGINT = -987654321
-products = []
+# n, w = map(int, input().split())
+# BIGINT = -987654321
+# products = []
 
-for _ in range(n):
-    products.append(list(map(str, input().split())))
+# for _ in range(n):
+#     products.append(list(map(str, input().split())))
 
-
+"""
 temparr = []
 def packing(count, weight, hope):
     print(count, weight, hope)
@@ -30,3 +30,28 @@ def packing(count, weight, hope):
 
 print(packing(0, 0, 0))
 print(temparr)
+
+완전탐색으로 최대 기대값은 출력했지만 뭘 담았는지는 못함
+
+"""
+import copy
+n, mainCapacity = map(int, input().split())
+
+volume = [-1 for _ in range(100)]
+need = [-1 for _ in range(100)]
+cache = [[-1 for _ in range(100)] for _ in range(1001)]
+
+def pack(capacity, item):
+    if item == n:
+        return 0
+    ret = copy.deepcopy(cache[capacity][item])
+    if ret != -1:
+        return ret
+    
+    ret = pack(capacity, item +1)
+
+    if capacity>= volume[item]:
+        ret = max(ret, pack(capacity - volume[item], item + 1) + need[item])
+    
+    return ret
+
