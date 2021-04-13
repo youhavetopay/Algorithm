122ff956ff1f50e60db7a6a7e6986fb29fa54acb
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-04-13 17:10:58 +0900

    종만북 9.2 동적프로그래밍
    코드 완성시킴
    
    만족도? 는 재귀로하고
    물품출력은 현재 만족도랑 다음 품목에 대한 만족도를 비교해서 함

--- 학교/종만북/9_2.py
+++ 학교/종만북/9_2.py
@@ -35,23 +35,53 @@ print(temparr)
 
 """
 import copy
-n, mainCapacity = map(int, input().split())
 
-volume = [-1 for _ in range(100)]
-need = [-1 for _ in range(100)]
-cache = [[-1 for _ in range(100)] for _ in range(1001)]
+names = ['' for _ in range(101)]
+info = [[0 for _ in range(2)] for _ in range(101)]
+cache = [[-1 for _ in range(1001)] for _ in range(101)]
 
-def pack(capacity, item):
-    if item == n:
+res = []
+
+
+n, w = map(int, input().split())
+
+for i in range(1, n+1):
+    a,b,c = map(str, input().split())
+    names[i] = a
+    info[i][0] = int(b)
+    info[i][1] = int(c)
+
+
+def choose(now, v):
+    if now > n:
         return 0
-    ret = copy.deepcopy(cache[capacity][item])
+    
+    ret = copy.deepcopy(cache[now][v])
+
     if ret != -1:
         return ret
     
-    ret = pack(capacity, item +1)
-
-    if capacity>= volume[item]:
-        ret = max(ret, pack(capacity - volume[item], item + 1) + need[item])
+    ret = choose(now +1, v)
+    if v+info[now][0] <= w:
+        ret = max(ret, info[now][1]+choose(now + 1, v+info[now][0]))
     
     return ret
 
+def setRes(start, v):
+    if start > n:
+        return 
+    
+    if choose(start, v) == choose(start +1, v):
+        setRes(start + 1, v)
+    
+    else:
+        res.append(names[start])
+        setRes(start +1, v + info[start][0])
+
+
+maxW = choose(0, 0)
+setRes(0,0)
+
+print(maxW, len(res))
+for temp in res:
+    print(temp)
