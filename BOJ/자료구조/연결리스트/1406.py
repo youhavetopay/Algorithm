26da79663bf3c1e489f36877f05361af5d95e3d4
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-05-24 14:38:27 +0900

    이중연결리스트 구현
    크흡...
    
    힘드네 ㅋㅋㅋ

--- BOJ/자료구조/연결리스트/1406.py
+++ BOJ/자료구조/연결리스트/1406.py
@@ -1,105 +1,105 @@
-# 에디터 
+# 에디터 이중연결리스트
 
 # 21년 여름 카카오 인턴 3번이랑 비슷함
 
 import sys
+from typing import Counter
 
 class Node():
     def __init__(self, data):
         self.data = data
-        self.next = None
+        self.left = None
+        self.right = None
     
 class LinkedList(object):
     def __init__(self):
         self.head = None
-        self.length = 0
+        self.cursor = None
     
 
-    def append(self, node):
-        self.length += 1
-        if self.head == None:
-            self.head = node
+    def append(self, data):
+        
+        if self.cursor == None:
+            self.head = data
+            self.cursor = data
         else:
-            cur = self.head
-            while cur.next != None:
-                cur = cur.next
-            cur.next = node
+            self.cursor.right = data
+            data.left =  self.cursor
+            data.right = None
+            self.cursor = data
     
     def getListData(self):
         cur = self.head
         returnStr = ''
-        while cur.next != None:
+        while cur.right != None:
             returnStr += cur.data
-            cur = cur.next
+            cur = cur.right
         returnStr += cur.data
         return returnStr
-    
 
-    def insertData(self, loc, data):
-        self.length += 1
-        cur_loc = 0
-        cur = self.head
-        preNode = None
-        if loc == 0:
-            data.next = cur
-        else:
-            while cur_loc < loc:
-                preNode = cur
-                cur = cur.next
-                cur_loc += 1
-            
-            preNode.next = data
-            data.next = cur
-    
-    def deleteData(self, loc):
-        self.length -= 1
-        cur_loc = 0
-        cur = self.head
-        preNode = None
-        nextNode = cur.next
-
-        if loc == 0:
-            self.head = nextNode
-        else:
-            while cur_loc < loc:
-                preNode = cur
-                cur = cur.next
-                nextNode = cur.next
-                cur_loc += 1
-            preNode.next = nextNode
+    def moveLeftCursor(self):
+        if self.cursor.left != None:
+            self.cursor = self.cursor.left
         
 
+    def moveRightCursor(self):
+        if self.cursor.right != None:
+            self.cursor = self.cursor.right
+
+    def insertData(self, data):
+        
+        if self.cursor.left == None:
+            self.head = data
+            data.right = self.cursor
+            self.cursor.left = data
+        else:
+            preNode = self.cursor.left
+            preNode.right = data
+            data.left = preNode
+            data.right = self.cursor
+            self.cursor.left = data
+
+
+    
+    def deleteData(self):
+        if self.cursor.left != None:
+            if self.cursor.left.left == None:
+                self.head = self.cursor
+                self.cursor.left = None
+            else:
+                preNode = self.cursor.left.left
+                preNode.right = self.cursor
+                self.cursor.left = preNode
+
+    
 
 input = sys.stdin.readline
 
 nowStr = input().strip()
 
-strList = []
 linkList = LinkedList()
 for i in nowStr:
     linkList.append(Node(i))
-
+linkList.append(Node("9"))
 orderCount = int(input().strip())
-loc = linkList.length
+
 
 for i in range(orderCount):
     orderStr = input().split()
     
     if orderStr[0] == 'L':
-        if loc != 0:
-            loc -= 1
+        linkList.moveLeftCursor()
     
     elif orderStr[0] == 'D':
-        if not(loc >= linkList.length):
-            loc += 1
+        linkList.moveRightCursor()
     
     elif orderStr[0] == 'B':
-        if loc != 0:
-            linkList.deleteData(loc)
-            loc -= 1
+        linkList.deleteData()
     
     elif orderStr[0] == 'P':
-        linkList.insertData(loc, Node(str(orderStr[1])))
-        loc = loc + 1
+        linkList.insertData(Node(str(orderStr[1])))
+        
+    #print(linkList.getListData(), linkList.cursor.data)
+    
 
-print(linkList.getListData())
+print(linkList.getListData()[:-1])
