27cfa6bd9252dc878fc4bd80ebef15fdcfab7f8f
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-08-19 17:57:49 +0900

    백준 미로찾기 2178 (BFS)
    최단 경로 방문은 BFS 해야 함
    
    파이썬은 x < y < z 이런거 됨
    나중에 이런 이동 문제 나오면 좌표 이동값을 배열로 선언해서 반복문으로 if문4개 되있는거 줄이기

--- BOJ/DFS_BFS/2178.py
+++ BOJ/DFS_BFS/2178.py
@@ -3,15 +3,69 @@
 n, m = map(int, input().split())
 
 board = []
-
+checkList = []
 for _ in range(n):
     tempList = list(input())
+    tempList2 = [False] * m
     board.append(tempList)
+    checkList.append(tempList2)
 
-queue = []
 
-nowLoc = [0,0]
+queue = [[0,0,1]]
 
-answer = 1
-while nowLoc != [n-1,m-1]:
+nowLoc = [0,0,1]
+
+checkList[nowLoc[1]][nowLoc[0]] = True
+
+while True:
+
+    nowLoc = queue.pop(0)
+
+    # 오른쪽
+    if nowLoc[0] + 1 <= m - 1 and board[nowLoc[1]][nowLoc[0]+1] != '0':
+        if not checkList[nowLoc[1]][nowLoc[0] + 1]:
+
+            queue.append([nowLoc[0]+1, nowLoc[1], nowLoc[2] + 1])
+            checkList[nowLoc[1]][nowLoc[0]+1] = True
+
+            if board[nowLoc[1]][nowLoc[0]+1] == '1':
+                board[nowLoc[1]][nowLoc[0]+1] = nowLoc[2] + 1
     
+    # 왼쪽
+    if nowLoc[0] - 1 >= 0 and board[nowLoc[1]][nowLoc[0]-1] != '0':
+        if not checkList[nowLoc[1]][nowLoc[0] - 1]:
+            
+            queue.append([nowLoc[0]-1, nowLoc[1], nowLoc[2] + 1])
+            checkList[nowLoc[1]][nowLoc[0]-1] = True
+
+            if board[nowLoc[1]][nowLoc[0]-1] == '1':
+                board[nowLoc[1]][nowLoc[0]-1] = nowLoc[2] + 1
+
+    # 아래  왼쪽 위에서 오른쪽 아래로 가는거라서
+    if nowLoc[1] + 1 <= n- 1 and board[nowLoc[1]+1][nowLoc[0]] != '0':
+        if not checkList[nowLoc[1] + 1][nowLoc[0]]:
+
+            queue.append([nowLoc[0], nowLoc[1] + 1, nowLoc[2]+ 1])
+            checkList[nowLoc[1]+1][nowLoc[0]] = True
+
+            if board[nowLoc[1]+1][nowLoc[0]] == '1':
+                board[nowLoc[1]+1][nowLoc[0]] = nowLoc[2] + 1
+
+    # 위
+    if nowLoc[1] - 1 >= 0 and board[nowLoc[1]-1][nowLoc[0]] != '0':
+        if not checkList[nowLoc[1] - 1][nowLoc[0]]:
+
+            queue.append([nowLoc[0], nowLoc[1] - 1, nowLoc[2]+ 1])
+            checkList[nowLoc[1]-1][nowLoc[0]] = True
+            
+            if board[nowLoc[1]-1][nowLoc[0]] == '1':
+                board[nowLoc[1]-1][nowLoc[0]] = nowLoc[2] + 1
+    print()
+    for temp in board:
+        print(temp)
+
+    if board[n-1][m-1] != '1':
+        break
+    
+
+print(board[n-1][m-1])
