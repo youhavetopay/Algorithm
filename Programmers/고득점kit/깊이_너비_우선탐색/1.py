cbdb9a43a557139d09f8b4f0ae7174f93866665a
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-09-29 16:47:23 +0900

    프로그래머스 고득점 킷 깊이/너비 우선탐색
    타겟넘버
    
    재귀함수로 품
    하나하나 +-하면서 모든 경우의수 검색해서 품  --> 완전탐색
    
    완전탐색안에 DFS랑 BFS가 속해있음
    
    조건식 잘 확인하기 !!!

--- Programmers/고득점kit/깊이_너비_우선탐색/1.py
+++ Programmers/고득점kit/깊이_너비_우선탐색/1.py
@@ -1,5 +1,42 @@
 # 타겟 넘버
 
+def makeSumNumber(sumValue,numbers, index, length ,targetNum, tempList):
+    count = 0
+    if sumValue == targetNum and index == length:
+        print(index, length, sumValue, tempList, '-------------------')
+        print('a')
+        return 1
+    else:
+        if index == length and sumValue != targetNum:
+            print(index, length, sumValue, tempList, 'ccccccccccc')
+            print('b')
+            return 0
+
+        else:
+            if index < length:
+                print(index, length, sumValue, 'aaaaaaaaa', tempList)
+                newList1 = tempList[:]
+                newList1.append(-numbers[index])
+                count += makeSumNumber(sumValue - numbers[index], numbers, index+1, length, targetNum,newList1)
+                
+                print(index, length, sumValue, 'b', tempList)
+                newList2 = tempList[:]
+                newList2.append(numbers[index])
+                count += makeSumNumber(sumValue + numbers[index], numbers, index+1, length, targetNum, newList2)
+                print(count, 'bbbbbbbbbb')
+
+    return count
+
+
 def solution(numbers, target):
     answer = 0
-    return answerbbb
+
+    answer = makeSumNumber(0, numbers, 0, len(numbers), target, [])
+
+    return answer
+
+
+n1 = [1, 1, 1, 1, 1]	
+n2 = [2,-2,2] 
+print(solution(n1, 3))
+print(solution(n2, 2))
--- Programmers/고득점kit/완전탐색/findPrime.py
+++ Programmers/고득점kit/완전탐색/findPrime.py
@@ -1,9 +1,24 @@
+import itertools
+
+def checkPrime(n):
+    i = 2
+    while i*i <= n:
+        if n % i == 0:
+            return False
+        i += 1
+    
+    return True
+        
+
 def solution(numbers):
     answer = 0
+    tempList = list(itertools.product(list(numbers), repeat=2))
+    print(tempList)
+    print(list(itertools.permutations(list(numbers))))
     return answer
 
 n1 = '17'
 n2 = '011'
 
 print(solution(n1))
-print(solution(n2))
+# print(solution(n2))
