8f639ecc67ec26b65772d6d6566b0677f76ce5f3
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-06-28 20:42:51 +0900

    백준 2798 블랙잭
    처음에 문제를 잘못봐서 어렵다고 생각했지만
    그냥 3중 반복문 돌려도 푸는 문제

--- BOJ/CLASS/CLASS2/CLASS2+/2798.py
+++ BOJ/CLASS/CLASS2/CLASS2+/2798.py
@@ -2,3 +2,14 @@ N, M = map(int, input().split(' '))
 
 numbers = list(map(int, input().split(' ')))
 
+answer = 0
+
+for i in range(N-2):
+    for j in range(i+1, N-1):
+        for z in range(j+1, N):
+            sum_value = numbers[i] + numbers[j] + numbers[z]
+            
+            if answer < sum_value <= M:
+                answer = sum_value
+
+print(answer)
