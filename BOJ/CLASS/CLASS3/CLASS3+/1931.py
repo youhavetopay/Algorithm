6a8a112c9a0d6f118aac323b17a7f4edb5221d55
DOUL_LGH <fldhsfk@gmail.com>
2022-07-07 14:47:30 +0900

    백준 1931 회의실 배정
    정렬문제
    회의가 끝나는 시간을 기준으로 정렬을 하고
    만약 끝나는 시간이 같다면 시작하는 시간이 빠른 걸로 정렬
    
    그 후 정렬된 배열을 반복문을 돌리면서
    체크
    
    문제를 자꾸 너무 어렵게 풀려고 함
    간단하게 생각하기

--- BOJ/CLASS/CLASS3/CLASS3+/1931.py
+++ BOJ/CLASS/CLASS3/CLASS3+/1931.py
@@ -1,10 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-
 time_table = []
+N = int(input())
 for i in range(N):
-    time_table.append(list(map(int, input().split(' '))))
+    time = list(map(int, input().split(' ')))
+    time_table.append(time)
 
-print(time_table)
+sorted_time_table = sorted(time_table, key=lambda x : (x[1], x[0]))
+answer = 1
+last_end_time = sorted_time_table[0][1]
+
+#print(sorted_time_table)
+for idx, time in enumerate(sorted_time_table):
+    if idx > 0:
+        if last_end_time <= time[0]:
+            last_end_time = time[1]
+            answer += 1
+
+print(answer)
