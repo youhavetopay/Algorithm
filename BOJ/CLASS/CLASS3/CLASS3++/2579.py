4b2a51fa8514bdcf0249a5aa48b3452abdaf6c1a
DOUL_LGH <fldhsfk@gmail.com>
2022-07-08 13:32:49 +0900

    백준 2579 계단 오르기
    DP 문제
    계단의 길이가 3이전까진 그냥 계산하고
    3이후 부턴 dp[i] = 전전칸에서 두칸올라와서 더하기 , 전전전칸 + 두칸뜀
    전칸 에서 올라온것
    둘중 최대값
    
    DP 문제 많이 풀어보기

---
+++ BOJ/CLASS/CLASS3/CLASS3++/2579.py
@@ -0,0 +1,39 @@
+import sys
+input = sys.stdin.readline
+
+step_count = int(input())
+
+step = []
+for _ in range(step_count):
+    step.append(int(input()))
+
+
+# step_count = 6
+# step = [
+#     10,
+#     20,
+#     15,
+#     25,
+#     10,
+#     20
+# ]
+
+def findMaxStepValue():
+
+    dp = [0] * (step_count)
+
+    dp[0] = step[0]
+    
+    if step_count >= 2:
+        dp[1] = dp[0] + step[1]
+        if step_count >= 3:
+            dp[2] = max(dp[0] + step[2], step[1] + step[2])
+
+            for i in range(3, step_count):
+                dp[i] = max(dp[i-3] + step[i-1] + step[i], dp[i-2] + step[i])
+                print(dp)
+        
+    print(dp[step_count-1])
+    return
+
+findMaxStepValue()
