22486c50f4570b445c83e0a41799784fc8d55dcd
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-11-19 00:38:36 +0900

    빠진 코드 추가ㅋㅋㅋ
    왜 저장을 안했었지? ㅋㅋㅋ

--- 해커랭크/자료구조/easy/DeleteDuplicate-valueNodesFromaSortedLinkedList.py
+++ 해커랭크/자료구조/easy/DeleteDuplicate-valueNodesFromaSortedLinkedList.py
@@ -5,3 +5,22 @@ class SinglyLinkedListNode:
 
 def removeDuplicates(llist):
     # Write your code here
+
+    pointer = llist
+    data = {}
+    while pointer != None:
+        data[pointer.data] = 1
+        pointer = pointer.next
+
+    head = None
+    last_node = head
+    for key, value in data.items():
+        if head == None:
+            head = SinglyLinkedListNode(key)
+            last_node = head
+        else:
+            new_node = SinglyLinkedListNode(key)
+            last_node.next = new_node
+            last_node = new_node
+
+    return head
