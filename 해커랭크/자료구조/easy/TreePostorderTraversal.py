83f0aebd8e9f663922f30333d9e2a2b47995b1d7
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-11-26 14:54:45 +0900

    해커랭크 Tree:Postorder Traversal
    후위순회
    L - R - C

---
+++ 해커랭크/자료구조/easy/TreePostorderTraversal.py
@@ -0,0 +1,17 @@
+def postOrder(root):
+    #Write your code here
+    data_list = appendData(root, [])
+    
+    print(' '.join(map(str, data_list)))
+
+def appendData(node, data_list):
+    if node.left != None:
+        data_list = appendData(node.left, data_list)
+
+    if node.right != None:
+        data_list = appendData(node.right, data_list)
+    
+    data_list.append(node.info)
+
+
+    return data_list
