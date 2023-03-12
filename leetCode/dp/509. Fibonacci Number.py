7347a100bff9786ed342b609958207167a4509fe
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-03-12 15:42:21 +0900

    LeetCode 509. Fibonacci Number 풀이코드 추가
    정말 다양한 풀이가 있는듯 ㅋㅋㅋ
    
    꾸준히 하자..!!

--- leetCode/dp/509. Fibonacci Number.py
+++ leetCode/dp/509. Fibonacci Number.py
@@ -1,3 +1,6 @@
+
+import collections
+
 class Solution:
 
     nums = [0, 1]
@@ -24,3 +27,46 @@ class Solution:
     '''
         책 풀이는 다음에..
     '''
+
+    def firstSoul(self, n: int) -> int:
+        if n <= 1:
+            return n
+        return self.firstSoul(n-1) + self.firstSoul(n-2)
+    
+
+    dp = collections.defaultdict(int)
+    def secondSoul(self, n: int) -> int:
+
+        if n <= 1:
+            return n
+        
+        if self.dp[n]:
+            return self.dp[n]
+        
+        self.dp[n] = self.secondSoul(n-1) + self.secondSoul(n-2)
+
+        return self.dp[n]
+    
+    def thirdSoul(self, n: int) -> int:
+        self.dp[0] = 0
+        self.dp[1] = 1
+
+        for i in range(2, n+1):
+            self.dp[i] = self.dp[i-1] + self.dp[i-2]
+
+        return self.dp[n]
+    
+    def fouthSoul(self, n: int) -> int:
+        x, y = 0, 1
+
+        for i in range(0, n):
+            x, y = y, x + y
+        
+        return x
+    
+    def fifthSoul(self, n: int) -> int:
+        M = np.matrix([[0,1], [1,1]])
+
+        vec = np.array([[0], [1]])
+
+        return np.matmul(M ** n, vec)[0]
