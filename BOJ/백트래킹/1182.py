2cc9e24c91df81a5889360492898651143670561
이군호 <fldhsfk@gmail.com>
2023-07-06 14:04:16 +0900

    백준 1182 부분수열의 합
    예전에 풀었던거 다시 풀어봄 ㅋㅋ
    
    예전에는 완전탐색으로 했는데
    이번엔 백트래킹?? 으로 해봄 ㅋㅋ
    메모리는 10배 더 썼지만 시간복잡도는 2배 줄임 ㅋㅋㅋㅋㅋ
    이게 맞나? ㅋㅋㅋ

--- BOJ/백트래킹/1182.py
+++ BOJ/백트래킹/1182.py
@@ -1,20 +1,57 @@
+import sys
+input = sys.stdin.readline
 
-N, S = map(int, input().split())
+# N, S = map(int, input().split())
 
-array = list(map(int, input().split()))
+# array = list(map(int, input().split()))
+
+N, S = 5, 0
+
+array = [-7, -3, -2, 5, 8]
 
 answer = 0
 
 
-# 이거 잘 기억하기
-# 모든 부분 수열 만들어주는 거임
-for i in range(1, 1 << N):
-    sumValue = 0
-    for j in range(N):
-        if i & (1 << j):
-            sumValue += array[j]
+# # 이거 잘 기억하기
+# # 모든 부분 수열 만들어주는 거임
+# for i in range(1, 1 << N):
+#     sumValue = 0
+#     for j in range(N):
+#         if i & (1 << j):
+#             sumValue += array[j]
     
-    if sumValue == S:
-        answer += 1
+#     if sumValue == S:
+#         answer += 1
 
+# print(answer)
+
+can = set()
+not_can = set()
+
+for i in range(N):
+    now = array[i]
+
+    def dfs(now_sum, seleted, last):
+        global N, answer
+        
+        key = tuple(sorted(seleted))
+        print(key)
+        if key in not_can:
+            return
+
+        if now_sum == S and key not in can:
+            can.add(key)
+            answer += 1
+        
+        for next in range(last+1, N):
+            if next not in seleted:
+                dfs(now_sum + array[next], seleted + [next], next)
+
+
+        not_can.add(key)
+
+    dfs(now, [i], -1)
+
+print(can)
+print(not_can)
 print(answer)
