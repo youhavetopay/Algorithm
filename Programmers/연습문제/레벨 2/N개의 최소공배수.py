c7033e9e14e9ac58bed5b9ac75344a4701960ff5
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-08-06 11:53:24 +0900

    프로그래머스 N개의 최소공배수
    다른 사람 풀이 추가
    
    문제 풀 시간이 없다..ㅋ ㅋㅋ

--- Programmers/연습문제/레벨 2/N개의 최소공배수.py
+++ Programmers/연습문제/레벨 2/N개의 최소공배수.py
@@ -33,3 +33,26 @@ def solution(arr):
                 nums[next_num] = 1
         
         i += 1
+
+
+def firstSolu(arr):
+
+    '''
+        다른 사람 풀이
+        https://velog.io/@insutance/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4Python-N%EA%B0%9C%EC%9D%98-%EC%B5%9C%EC%86%8C%EA%B3%B5%EB%B0%B0%EC%88%98
+
+        최소공배수를 누적해서?? 계속 구해나감
+        
+        최소공배수는 x * y // gcd(x, y)
+
+        gcd 는 최대공약수
+    '''
+
+    from math import gcd
+
+    answer = arr[0]
+
+    for num in arr:
+        answer = answer * num // gcd(answer, num)
+    
+    return answer
