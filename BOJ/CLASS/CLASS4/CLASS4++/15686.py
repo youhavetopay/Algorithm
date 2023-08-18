8cbf81fad1bfdf600843224cb63d6e85d25323c4
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-08-18 13:44:08 +0900

    백준 15686 치킨 배달
    solved.ac CLASS 4 ++ 골드 5 문제
    
    치킨집 중 N개를 선택했을때 도시의 치킨거리가 최소가 되는 값을 구하는 문제
    
    조합을 구할 수 있으면 쉽게 풀 수 있는 문제
    
    골드 2~3 이런거 풀다가 골드 5 푸니까 디게 쉽게 느껴짐 ㅋㅋㅋㅋ
    
    열심히 하자.>!!

---
+++ BOJ/CLASS/CLASS4/CLASS4++/15686.py
@@ -0,0 +1,64 @@
+
+'''
+    백준 15686. 치킨 배달
+    치킨집 중 N개를 선택했을때 도시의 치킨거리가 최소가 되는 값을 구하는 문제
+'''
+
+import sys
+input = sys.stdin.readline
+
+from itertools import combinations
+
+def solution():
+
+    '''
+        나의 풀이
+
+        나의 접근법
+        처음엔 BFS를 해야하나 생각했었는데
+        그럴 필요가 없었음.. ㅋㅋㅋ
+
+        일단 집 위치랑 치킨집 위치를 따로 저장해두고
+        조합을 통해서 현재 선택한 치킨집의 인덱스에 따라 도시의 치킨 거리를 계산해주면서
+        비교하면 풀리는 문제였음 ㅎㅎ
+
+        집 수는 최대 100개 이고 치킨집 수도 최대 13개 이기 때문에 
+        for 문으로도 쉽게 쉽게 풀리는 듯 함 ㅎㅎ
+    '''
+
+    N, M = map(int, input().split())
+
+
+    houses = []
+    chickens = []
+
+    for y in range(N):
+        row = list(map(int, input().split()))
+        
+        for x, kind in enumerate(row):
+            if kind == 1:
+                houses.append([x, y])
+            elif kind == 2:
+                chickens.append([x, y])
+
+    total_min_chicken_dist = float('inf')
+    for selected_chicken in list(combinations([ i for i in range(len(chickens))], M)):
+
+        now_min_chicken_dist = 0
+
+        for house in houses:
+
+            chicken_dist = float('inf')
+
+            for chicken_i in selected_chicken:
+                chicken_dist = min(chicken_dist, abs(house[0] - chickens[chicken_i][0]) + abs(house[1] - chickens[chicken_i][1]))
+
+            
+            now_min_chicken_dist += chicken_dist
+        
+        total_min_chicken_dist = min(total_min_chicken_dist, now_min_chicken_dist)
+
+    print(total_min_chicken_dist)
+    return
+
+solution()
