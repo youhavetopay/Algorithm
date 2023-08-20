b8a54e1539cd5d13c6b5054e7ab5fb52125cfcb1
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2023-08-20 22:26:52 +0900

    백준 15686. 치킨 배달
    다른 사람 풀이 추가
    
    문제 풀 시간이 없다...ㅋ
    
    열심히 하자..!

--- BOJ/CLASS/CLASS4/CLASS4++/15686.py
+++ BOJ/CLASS/CLASS4/CLASS4++/15686.py
@@ -62,3 +62,42 @@ def solution():
     return
 
 solution()
+
+def firstSolu():
+
+    '''
+        다른 사람 풀이
+        https://codesyun.tistory.com/185
+
+        나랑 똑같은 풀이 ㅋㅋㅋㅋ
+        combinations 사용해서 똑같이 풀어냄 ㅋㅋㅋ
+    '''
+
+    n, m = map(int, input().split())
+    city = list(list(map(int, input().split())) for _ in range(n))
+
+    result = 999999
+    house = []
+    chick = []
+
+    for i in range(n):
+        for j in range(n):
+            if city[i][j] == 1:
+                house.append([i, j])
+            elif city[i][j] == 2:
+                chick.append([i, j])
+    
+
+    for chi in combinations(chick, m):
+        temp = 0
+
+        for h in house:
+            chi_len = 999
+            for j in range(m):
+                chi_len = min(chi_len, abs(h[0] - chi[j][0]) + abs(h[1] - chi[j][i]))
+            
+            temp += chi_len
+        
+        result = min(result, temp)
+
+    print(result)
